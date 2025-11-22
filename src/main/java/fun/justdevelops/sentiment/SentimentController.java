f97aa992ad39e613394e5f9c2c1cbddff1d0a96c
Alexey <alex-mur@outlook.com>
2025-11-22 20:39:58 +0300

    Изменения в логике sentiment

--- src/main/java/fun/justdevelops/sentiment/SentimentController.java
+++ src/main/java/fun/justdevelops/sentiment/SentimentController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.random.RandomGenerator;
-
 @RestController
 @RequestMapping("/api")
 public class SentimentController {
